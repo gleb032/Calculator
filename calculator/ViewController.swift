49e2a776451fc8562c385e5cca7d1871005e589a
Gleb Fandeev <glebfandeev@yandex-team.ru>
2022-08-24 18:28:17 +0300

    Initial commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Gleb Fandeev on 24.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
