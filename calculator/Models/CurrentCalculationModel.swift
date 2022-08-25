ff37f33be0cbb72e0e922fd1c406b62b41841439
Gleb Fandeev <glebfandeev@yandex-team.ru>
2022-08-25 19:14:49 +0300

    Added some calculation model logic

--- calculator/Models/CurrentCalculationModel.swift
+++ calculator/Models/CurrentCalculationModel.swift
@@ -12,10 +12,59 @@ struct Calculation {
   var firstNumber: Double = 0
   var secondNumber: Double?
   var operation: Operation?
+
+  mutating func doOperation(button: Button) {
+    switch button {
+    case .number(let num):
+      if let _ = operation {
+        secondNumber = num
+        break
+      }
+      if let x = secondNumber {
+        secondNumber = x * 10 + num
+      } else {
+        firstNumber = firstNumber * 10 + num
+      }
+    case .operation(let operation):
+      self.operation = operation
+    case .equal:
+      firstNumber = getResult(operation: operation)
+    }
+  }
+  func getResult(operation: Operation?) -> Double {
+    guard let operation = operation else {
+      return firstNumber
+    }
+    if operation == .procent {
+      return firstNumber * 0.01
+    }
+    guard let secondNumber = secondNumber else {
+      return firstNumber
+    }
+    switch operation {
+    case .add:
+      return firstNumber + secondNumber
+    case .subtract:
+      return firstNumber - secondNumber
+    case .multiply:
+      return firstNumber * secondNumber
+    case .divide:
+      return firstNumber / secondNumber
+    case .procent:
+      fatalError("Something went wrong")
+    }
+  }
+
 }
 
 enum Operation {
-  case add, subtract, multiply, divide
+  case add, subtract, multiply, divide, procent
 }
 
-// Operation logic here ? 
+// Operation logic here ?
+
+enum Button {
+  case number(Double)
+  case operation(Operation)
+  case equal
+}
